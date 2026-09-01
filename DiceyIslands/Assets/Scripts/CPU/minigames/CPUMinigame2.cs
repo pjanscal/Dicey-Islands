0cda67370e1e1f42efa5dcd314931c2aca5a8f7f
DDDSchool <97126717@st.deltion.nl>
2026-09-01 11:19:05 +0200

    CPUminigame2 v1
    can now think or they should press it

--- DiceyIslands/Assets/Scripts/CPU/minigames/CPUMinigame2.cs
+++ DiceyIslands/Assets/Scripts/CPU/minigames/CPUMinigame2.cs
@@ -1,18 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CPUMinigame2: CPUMangeren
 {
-    //if hardcoded robert fault :3
 
     [Serializable]
     private class DifficultyCPUConfigs
     {
+        //ik a class in a class in a class
+        [Serializable]
+        public class PointChanceToSelect
+        {
+            public int pointGain;
+            public float chanceToSelect;
+        }
+
         public Vector2 offSet;
-        //wa happend if it is that point
         public GameMangeren.CPUDifficulty difficulty;
+
+        [Header("pointChange to select")]
+        public List<PointChanceToSelect> pointChanceToSelects = new();
+        [HideInInspector] public Dictionary<int, float> pointsChance = new();
     }
 
     //objects needed
@@ -28,9 +40,15 @@ public class CPUMinigame2: CPUMangeren
         base.Start();
 
         //beta
-        foreach(DifficultyCPUConfigs difficultyCPUConfigs in difficultiesConfigsEdit)
+        foreach (DifficultyCPUConfigs difficultyCPUConfigs in difficultiesConfigsEdit)
         {
             difficultiesConfigs.Add(difficultyCPUConfigs.difficulty, difficultyCPUConfigs);
+
+            //set up the dicenary of pointchance
+            foreach (DifficultyCPUConfigs.PointChanceToSelect pointChanceToSelect in difficultyCPUConfigs.pointChanceToSelects)
+            {
+                difficultyCPUConfigs.pointsChance.Add(pointChanceToSelect.pointGain, pointChanceToSelect.chanceToSelect);
+            }
         }
     }
 
@@ -39,10 +57,16 @@ public class CPUMinigame2: CPUMangeren
         LokaalConnecter.PlayerController playerController = LokaalConnecter.plrsController[plrId];
         DifficultyCPUConfigs configs = difficultiesConfigs[GameMangeren.cPUDifficulty];
 
+        int oldPointGain = int.MaxValue;
+
         //ready up
         yield return new WaitForSeconds(secBeforeReadyUp);
 
         playerController.CPUSetButton(LokaalConnecter.InputType.x, true);
+
+        yield return null;
+
+        playerController.CPUSetButton(LokaalConnecter.InputType.x, false);
 
         //soon unitl it is finish :3
         while (true)
@@ -50,14 +74,33 @@ public class CPUMinigame2: CPUMangeren
             //caculate so we have less time between the real offset
             float offset = UnityEngine.Random.Range(configs.offSet.x, configs.offSet.y);
 
-            yield return new WaitUntil(() => spawnObject.isActiveAndEnabled);
+            //bbug if it go the same or no one press nothing happend
+            yield return new WaitUntil(() => spawnObject.objectToActivate == null || !spawnObject.objectToActivate.activeSelf || oldPointGain != getPointGain());
+            yield return new WaitUntil(() => spawnObject.objectToActivate != null && spawnObject.objectToActivate.activeSelf);
 
             //now it spawned in and it wait for the offset while caculating if it want it
+            int pointGain = getPointGain();
+            oldPointGain = pointGain;
+            float chanceToSelect = configs.pointsChance[pointGain];
+            float rngChance = UnityEngine.Random.Range(0f, 1f); //1 is th emax
+
+            if (rngChance > chanceToSelect) continue;
+
             yield return new WaitForSeconds(offset);
 
             playerController.CPUSetButton(LokaalConnecter.InputType.x, true);
+
+            yield return null;
         }
 
         //yield return null;
+    }
+
+    int getPointGain()
+    {
+        Match pointMatch = Regex.Match(spawnObject.objectToActivate.name, @"-?\d+");
+        if (!pointMatch.Success) return -3; //if it false jut return the smalest of them all
+
+        return int.Parse(pointMatch.Value);
     }
 }
