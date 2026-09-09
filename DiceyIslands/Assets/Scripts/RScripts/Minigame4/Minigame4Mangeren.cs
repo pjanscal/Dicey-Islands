bf6601daf3cffec517930bad51fb7bcc30e2cd05
DDDSchool <97126717@st.deltion.nl>
2026-09-09 13:24:10 +0200

    minigame4 v.5
    added some logic to who died

--- DiceyIslands/Assets/Scripts/RScripts/Minigame4/Minigame4Mangeren.cs
+++ DiceyIslands/Assets/Scripts/RScripts/Minigame4/Minigame4Mangeren.cs
@@ -1,27 +1,88 @@
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public class Minigame4Mangeren : MonoBehaviour
 {
     public static Minigame4Mangeren instance;
 
-    public bool isActive = false;
+    [HideInInspector] public bool isActive = false;
+    [HideInInspector] public Dictionary<int, Player_Minigame4> plrScripts = new();
+    private HashSet<int> plrsIngame = new();
+    private int potatoTarget = 0; //0 is like no one work as 1 if that plr is dead
+    private bool canGivePotato = false;
+
+    public Transform camaraHolder;
+
+    //configs
+    private Vector2 potatoLifeTime = new Vector2(14, 20);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         instance = this;
         GameMangeren.startMiniGame += Init;
+
+        for (int plrId = 1; plrId <= LokaalConnecter.maxPlr; plrId++)
+        {
+            plrsIngame.Add(plrId);
+        }
     }
 
     //init when it start
     public void Init()
     {
         print("start minigame4");
+        isActive = true; //say u can use it
+
+        Invoke("GiveRandomPlrPotato", 3f); //starting time soon make a countdown
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator PlayPotatoLifeTime()
+    {
+        float rngTime = UnityEngine.Random.Range(potatoLifeTime.x, potatoLifeTime.y);
+        yield return new WaitForSeconds(rngTime);
+
+        //kill the one with potato
+        canGivePotato = false;
+        plrsIngame.Remove(potatoTarget);
+        print($"player{potatoTarget} is out of the game");
+
+        GiveRandomPlrPotato();
+    }
+
+    void GivePlrPotato(int plrId)
+    {
+        //put the old potato inside the player;
+        potatoTarget = plrId;
+    }
+
+    void PlayerCollideWithOtherPlayer(int owner, int target)
+    {
+        if (potatoTarget != owner || !canGivePotato) return; //then it wouold be useless the script
+
+        GivePlrPotato(target);
+    }
+
+    void GiveRandomPlrPotato()
+    {
+        //get random player
+        int rng = UnityEngine.Random.Range(1, plrsIngame.Count);
+        int plrId = plrsIngame.ElementAt(rng);
+        print($"player{plrId} have potato");
+
+        //set it up
+        canGivePotato = true;
+        GivePlrPotato(plrId);
+
+        //start it
+        StartCoroutine(PlayPotatoLifeTime());
     }
 }
--- DiceyIslands/Assets/Scripts/RScripts/Minigame4/Player_Minigame4.cs
+++ DiceyIslands/Assets/Scripts/RScripts/Minigame4/Player_Minigame4.cs
@@ -5,12 +5,15 @@ public class Player_Minigame4 : MonoBehaviour
     [HideInInspector] public float speed;
 
     //configs
-    private float defaultSpeed = 5f;
+    [SerializeField] private int PlrId;
+
+    const float defaultSpeed = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        speed = defaultSpeed;
+        Minigame4Mangeren.instance.plrScripts.Add(PlrId, this);
     }
 
     // Update is called once per frame
@@ -24,6 +27,20 @@ public class Player_Minigame4 : MonoBehaviour
     //move the player
     void Move()
     {
-        
+        LokaalConnecter.PlayerController playerController = LokaalConnecter.plrsController[PlrId];
+
+        Vector2 moveDir = playerController.GetMoveDir();
+        if (moveDir == Vector2.zero) return;
+
+        //move the plr
+        Vector3 forward = Minigame4Mangeren.instance.camaraHolder.forward;
+        forward.y = 0; //stop going up/down
+        forward.Normalize();
+
+        Vector3 right = Minigame4Mangeren.instance.camaraHolder.right;
+        right.y = 0; //stop going up/down
+        right.Normalize();
+
+        transform.position += (forward * moveDir.y + right * moveDir.x) * speed * Time.deltaTime;
     }
 }
