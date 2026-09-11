1b555a34198e2cb2321029d7e68d52ca1de0d9e2
piclepumkinprinces <robertdenhond2009@gmail.com>
2026-09-11 14:28:25 +0200

    minigame V5.1

---
+++ DiceyIslands/Assets/Scripts/RScripts/Minigame5/MaingameScript5.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MaingameScript5 : MonoBehaviour
+{
+    [SerializeField] int plrId;
+    LokaalConnecter.PlayerController playerController;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerController = LokaalConnecter.plrsController[plrId];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController == null || !playerController.occuplied) return;
+    }
+}
